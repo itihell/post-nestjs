74cdd6535aadc6064dd8e08ea87c3314f9325883
Itihell Mejia <itihell.mejia@gmail.com>
2021-12-21 15:26:51 -0600

    validaciones personalizadas para los enum

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,4 +1,5 @@
 import { IsArray, IsBoolean, IsEnum, IsString } from 'class-validator';
+import { EnumToString } from 'src/helpers/enumToString';
 import { PostCategory } from '../enums';
 
 export class CreatePostDto {
@@ -12,7 +13,9 @@ export class CreatePostDto {
   excerpt: string;
   @IsString()
   content: string;
-  @IsEnum(PostCategory)
+  @IsEnum(PostCategory, {
+    message: `Opcion invalida las opciones son ${EnumToString(PostCategory)}`,
+  })
   category: PostCategory;
   @IsArray()
   @IsString({ each: true })
