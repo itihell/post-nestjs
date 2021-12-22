8e1213df80b9e726a889fe8660d193ab879bc2f3
Itihell Mejia <itihell.mejia@gmail.com>
2021-12-22 10:34:17 -0600

    Configurando el env de conexion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { DATABASE_PASSWORD, DATABASE_NAME } from './config/constants';
+import {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USERNAME,
+} from './config/constants';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345678',
-      database: 'post',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>(DATABASE_HOST),
+        port: parseInt(config.get<string>(DATABASE_PORT), 10),
+        username: config.get<string>(DATABASE_USERNAME),
+        password: config.get<string>(DATABASE_PASSWORD),
+        database: config.get<string>(DATABASE_NAME),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        logger: 'file',
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
     }),
     PostModule,
     UserModule,
---
+++ src/config/constants.ts
@@ -0,0 +1,9 @@
+/***
+ * Constanctes de las variables de entorno para conexión a la base de datos
+ *
+ **/
+export const DATABASE_HOST = 'DATABASE_HOST';
+export const DATABASE_PORT = 'DATABASE_PORT';
+export const DATABASE_USERNAME = 'DATABASE_USERNAME';
+export const DATABASE_PASSWORD = 'DATABASE_PASSWORD';
+export const DATABASE_NAME = 'DATABASE_NAME';
